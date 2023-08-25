1ba847224e8a21465017ef1466caf51f0a15fb31
Aurélien Manteaux <amanteaux@coreoz.com>
2023-08-25 13:27:44 +0200

    Add sample tests

---
+++ src/test/java/com/coreoz/SampleTest.java
@@ -0,0 +1,11 @@
+package com.coreoz;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class SampleTest {
+    @Test
+    public void methodToTest__test_scenario_description() {
+        Assertions.assertThat(1).isEqualTo(1);
+    }
+}
